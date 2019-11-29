7fdd5eedf266393889d2104d2d2f6cfb64b8bb53
Liliia Kondurova <kondurovaliliia@gmail.com>
2019-11-29 23:48:52 +0300

    Added checkbox and delete icon

--- Planner/main.js
+++ Planner/main.js
@@ -1,6 +1,15 @@
 function addTask(){
     var newTask = document.createElement('li');
-    newTask.innerHTML = document.getElementById('taskField').value;
+    var newTaskText=document.createElement('span');
+    var newTaskCheckbox=document.createElement('input');
+    var newTaskClose=document.createElement('i');
+    newTaskCheckbox.setAttribute('type','checkbox');
+    newTaskClose.setAttribute('class','material-icons');
+    newTaskClose.innerHTML='close';
+    newTaskText.innerHTML = document.getElementById('taskField').value;
     document.getElementById('todoList').appendChild(newTask);
+    newTask.appendChild(newTaskCheckbox);
+    newTask.appendChild(newTaskText);
+    newTask.appendChild(newTaskClose);
     document.getElementById('taskField').value="";
 }
