c5f72aaf6131db78b1b95eda83b4eb61cff52020
Liliia Kondurova <kondurovaliliia@gmail.com>
2019-11-30 14:33:01 +0300

    Added remove To-Do List item functionality

--- Planner/main.js
+++ Planner/main.js
@@ -5,6 +5,7 @@ function addTask(){
     var newTaskClose=document.createElement('i');
     newTaskCheckbox.setAttribute('type','checkbox');
     newTaskClose.setAttribute('class','material-icons');
+    newTaskClose.setAttribute('onclick','removeListItem()');
     newTaskClose.innerHTML='close';
     newTaskText.innerHTML = document.getElementById('taskField').value;
     document.getElementById('todoList').appendChild(newTask);
@@ -12,4 +13,19 @@ function addTask(){
     newTask.appendChild(newTaskText);
     newTask.appendChild(newTaskClose);
     document.getElementById('taskField').value="";
+}
+
+function removeListItem(){
+    var items=document.getElementsByTagName('li');
+    var tab=[];
+    var liIndex;
+    for (var i=0; i<items.length; i++){
+        tab.push(items[i].innerHTML);
+    }
+    for (var i=0; i<items.length; i++){
+        items[i].onclick=function(){
+            liIndex=tab.indexOf(this.innerHTML);
+            items[liIndex].parentNode.removeChild(items[liIndex]);
+        }
+    }
 }
